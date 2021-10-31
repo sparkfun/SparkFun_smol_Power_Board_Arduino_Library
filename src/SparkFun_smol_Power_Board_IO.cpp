5fe099c160d15da227bb3af1fe650e9cf9bbd9e0
Paul <pc235603@gmail.com>
2021-10-31 11:01:18 +0000

    Update product SKUs

--- src/SparkFun_smol_Power_Board_IO.cpp
+++ src/SparkFun_smol_Power_Board_IO.cpp
@@ -8,9 +8,9 @@
  * This library facilitates communication with the smôl Power Board over I<sup>2</sup>C.
  * 
  * Want to support open source hardware? Buy a board from SparkFun!
- * <br>SparkX smôl Power Board AAA (SPX-18360): https://www.sparkfun.com/products/18360
- * <br>SparkX smôl Power Board LiPo (SPX-18359): https://www.sparkfun.com/products/18359
- * <br>SparkX smôl ESP32 (SPX-18362): https://www.sparkfun.com/products/18362
+ * <br>SparkX smôl Power Board LiPo (SPX-18622): https://www.sparkfun.com/products/18622
+ * <br>SparkX smôl Power Board AAA (SPX-18621): https://www.sparkfun.com/products/18621
+ * <br>SparkX smôl ESP32 (SPX-18619): https://www.sparkfun.com/products/18619
  * 
  * @section author Author
  * 
